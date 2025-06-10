968073352bd0203add374f2a54d658adf1a4bcac
Kyle Forsberg <kyle.forsberg@marquette.edu>
2025-06-10 10:53:03 -0500

    few comments

--- PartsSearch2/FCPscraper.cs
+++ PartsSearch2/FCPscraper.cs
@@ -35,30 +35,34 @@ public class FCPscraper
 
         //successful search, return LINKS to every product page of a match
     }
-    
-    
+
+
     // PLEASE FINISH THE METHOD BELOW THANKS =====================================================================
+    // Also test implementation above !!!
     
+    
+
+
     public List<Listing>? FindPricesFCP(List<string> Links)
     {
         //this takes each of the links, finds the prices and other bits of it
         //organizes them into the listing class
         //and returns a list of those listings
-        
+
         //null list check
-        if(Links.Count==0){Console.WriteLine("Null link, check that error to ensure Find Price is not called with a null link");}
-        
+        if (Links.Count == 0) { Console.WriteLine("Null link, check that error to ensure Find Price is not called with a null link"); }
+
         var web = new HtmlWeb();
         List<Listing> results = new List<Listing>();
         //load page and init list;
-        
-        
-        foreach(string link in Links)
+
+
+        foreach (string link in Links)
         {
             //main looping around FCP euro
-            
+
         }
         return results;
-        
+
     }
 }
